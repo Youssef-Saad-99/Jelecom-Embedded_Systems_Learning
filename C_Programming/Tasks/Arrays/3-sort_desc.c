0caab0eddda4ab660bd37e93dce4b4df9ff5bfa4
Youssef-Mohamed-99 <youssefmohamed4004306@gmail.com>
2025-08-12 06:17:47 +0300

    Added 3-sort_desc.c file

---
+++ C_Programming/Tasks/Arrays/3-sort_desc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void sort_desc(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 2, 9, 1, 6};
+    int size = 5;
+    sort_desc(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
